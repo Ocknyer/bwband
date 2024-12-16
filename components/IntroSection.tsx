0528de05e1837247dee0ceec21fea566e50bbbe9
Ocknyer <kdu4045@naver.com>
2024-12-16 10:41:46 +0900

    메인 레이아웃 수정

--- components/IntroSection.tsx
+++ components/IntroSection.tsx
@@ -4,7 +4,7 @@ import DoubleArrowDown from './Common/DoubleArrowDown';
 
 const IntroSection = () => {
   return (
-    <section className='flex flex-col min-h-dvh justify-between pt-40 sm:pt-48 pb-20 sm:pb-36'>
+    <section className='flex flex-col min-h-dvh justify-between pt-40 sm:pt-48 pb-20 sm:pb-36 w-full'>
       <Fade direction='up' triggerOnce duration={1000}>
         <div className='flex flex-col gap-4 items-center text-center font-nanumMyeongjo'>
           <p className='text-base font-bold text-white leading-8'>흑과 백, 글과 종이이자 음표와 악보.</p>
@@ -15,9 +15,12 @@ const IntroSection = () => {
             대비되는 두 색이 조화를 만드는 그 현장으로 <br className='block sm:hidden' /> 여러분을 초대합니다.
           </p>
         </div>
-        <div className='flex flex-col gap-2 items-center text-center'>
-          <p className='font-capsSmall text-4xl sm:text-5xl'>The Rotten</p>
-          <p className='font-capsSmall text-4xl sm:text-5xl'>Band JANSs</p>
+        <div className='flex flex-col gap-2 items-center text-center w-full'>
+          <p className='font-bold text-sm'>LINE UP</p>
+          <div className='flex text-center w-full'>
+            <p className='flex-1 font-capsSmall text-4xl sm:text-5xl text-primary'>Band JANSs</p>
+            <p className='flex-1 font-capsSmall text-4xl sm:text-5xl text-white'>The Rotten</p>
+          </div>
           <Fade triggerOnce delay={1000} className='mt-4'>
             <DoubleArrowDown />
           </Fade>
